e4ef4ed0cf347f17866a7d40901b3ae6c87caa6a
Davidoffili <palagashvilid@gmail.com>
2020-06-28 19:25:45 -0700

    continued building out file structure for auth modules and set up index.js, ready for router!

---
+++ src/server.js
@@ -0,0 +1,22 @@
+'use strict';
+// Bring in the middleware to be used by the server
+const express = require('express');
+
+//require router module just like in our last lab, except is lives in a different folder.
+const authenticationRouter = require('./auth/router.js');
+
+const app = express();
+// app.use() each them in our app so that our routes will respond.
+app.use(express.json());
+
+app.get('/', authenticationRouter);
+
+// exports as object to be triggered instead of starting on its own.
+module.exports = {
+    server: app,
+    start: (port) => {
+        app.listen(port, () => {
+            console.log('Our server is running on port: ' + port);
+        });
+    }
+};
