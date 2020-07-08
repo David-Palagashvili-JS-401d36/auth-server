66f22fd3585cbb990ffb12ffa47161d4144c10be
Davidoffili <palagashvilid@gmail.com>
2020-07-07 21:20:32 -0700

    completed createUser async function

--- src/auth/router.js
+++ src/auth/router.js
@@ -6,11 +6,29 @@ const router = express.Router();
 const UserModel = require('./models/users-model.js');
 const User = new UserModel();
 
-router.post('/signup', newUser);
+router.post('/signup', createUser);
 router.post('/signin', auth, UserSignIn);
 router.get('/users', getUsers);
 
-async function signUp(request, response) {}
+async function createUser(request, response) {
+    let userExists = await User.exists( {username: request.body.username} );
+    if (userExists) {
+        response.send('user already exists');
+        return;
+    }
+    let newUser = await UserModel.hashPassword(request.body.password);
+    let newUser = await User.create( {username: request.body.username, password: password} );
+    if (newUser) {
+        let token = UserModel.generateToken( {username: request.body.username} )
+        // console.log(UserModel.generateToken({ username: request.body.username }));
+        console.log(token);
+        response.cookie('token', token);
+        response.header('token', token);
+        response.send('user was signed up');
+    } else {
+        response.status(403).send('that user is invalid');
+    }
+};
 
 async function UserSignIn(request, response) {}
 
