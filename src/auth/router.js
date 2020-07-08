e583520965d6768681cfbb71b13b745d0012756b
Davidoffili <palagashvilid@gmail.com>
2020-07-07 19:10:13 -0700

    brought in middleware and instatiated user model on router.js

--- src/auth/router.js
+++ src/auth/router.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const express = require('express');
+const auth = require('./middleware/basic.js');
+const router = express.Router();
+const UserModel = require('./models/users-model.js');
+const User = new UserModel();
