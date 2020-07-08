e96a13144490bcd86c098a65ae3e9ddccac312ab
Davidoffili <palagashvilid@gmail.com>
2020-07-07 19:47:05 -0700

    set up async functions for the three routes

--- src/auth/router.js
+++ src/auth/router.js
@@ -5,3 +5,13 @@ const auth = require('./middleware/basic.js');
 const router = express.Router();
 const UserModel = require('./models/users-model.js');
 const User = new UserModel();
+
+router.post('/signup', newUser);
+router.post('/signin', auth, UserSignIn);
+router.get('/users', getUsers);
+
+async function signUp(request, response) {}
+
+async function UserSignIn(request, response) {}
+
+async function getUsers(request, response) {}
