5d000611be3fa42216bb2bb130ec746e057a7213
Davidoffili <palagashvilid@gmail.com>
2020-07-12 16:16:19 -0700

    error handler for missing headers and extraction of token, completed

--- src/auth/middleware/bearer-auth.js
+++ src/auth/middleware/bearer-auth.js
@@ -1,9 +1,15 @@
 'use strict';
 
-const UserModel = require('../models/users-model.js');
+const UserModel = require('../models/user-model.js');
 
 // TODO: Using an async fn, set up the logic for bearer auth. 
-// Instant 401 if no auth headers are passed in.
-// split the auth headers to extract auth type and token
+
 // validate the token 
 // export as module
+
+async function bearerAuth(request, response, next) {
+    if (!request.headers.authorization) { // Instant 401 if no auth headers are passed in.
+        response.status(401).send('Woah there, we didn\'t get any authorization headers from you!');
+    }
+    let [authType, token] = request.headers.authorization.split(' '); // split the auth headers to extract token
+};
