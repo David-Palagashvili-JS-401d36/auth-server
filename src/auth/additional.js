8e504a87e96fa24e6c2907af443070fcf3af1879
Davidoffili <palagashvilid@gmail.com>
2020-07-12 19:07:51 -0700

    refactoring, set up TODOs for additional routes with RBAC

--- src/auth/additional.js
+++ src/auth/additional.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const router = express.Router();
 
-// bearer auth module
+// bearer auth middleware
 const bearer = require('./middleware/bearer-auth.js');
 
 // instantiate user model
@@ -13,6 +13,24 @@ const User = new UserModel();
 
 router.get('/secret', bearer, (req, res) => {
     res.send(req.user);
+});
+
+// TODO: wire up the following routes as a means of ensuring that our authorization system works well:
+
+router.get('/read', bearer, permissions('read'), (req, res) => {
+
+});
+
+router.post('/add', bearer, permissions('create'), (req, res) => {
+    
+});
+
+router.put('/change', bearer, permissions('update'), (req, res) => {
+    
+});
+
+router.delete('/remove', bearer, permissions('delete'), (req, res) => {
+    
 });
 
 module.exports = router;
